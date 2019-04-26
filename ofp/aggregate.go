2d52eb8fcdee6d27ec193a227cb063600590fd75
Alexandr Kostrikov <alexandr.kostrikov@gmail.com>
2019-04-26 12:15:46 +0300

    Fix internal

--- ofp/aggregate.go
+++ ofp/aggregate.go
@@ -3,7 +3,7 @@ package ofp
 import (
 	"io"
 
-	"github.com/netrack/openflow/internal/encoding"
+	"github.com/akostrikov/openflow/internal/encoding"
 )
 
 // AggregateStatsRequest is a multipart request used to aggregate
